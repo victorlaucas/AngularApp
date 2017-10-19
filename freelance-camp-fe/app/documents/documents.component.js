472d743dc75f8f57f9956918d7ce4afa7ce72de6
Victor <victorlaucas@gmail.com>
2017-10-18 18:45:35 -0600

    Created mock documents and showed those documents on the navigation page

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -9,6 +9,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var DocumentsComponent = /** @class */ (function () {
     function DocumentsComponent() {
+        this.documents = [
+            {
+                title: "My First Doc",
+                description: "Content",
+                file_url: 'http://google.com',
+                updated_at: '10/18/17',
+                image_url: 'http://google.com'
+            },
+            {
+                title: "My Second Doc",
+                description: "Content",
+                file_url: 'http://google.com',
+                updated_at: '10/18/17',
+                image_url: 'http://google.com'
+            },
+            {
+                title: "My Last Doc",
+                description: "Content",
+                file_url: 'http://google.com',
+                updated_at: '10/18/17',
+                image_url: 'http://google.com'
+            }
+        ];
     }
     DocumentsComponent = __decorate([
         core_1.Component({
